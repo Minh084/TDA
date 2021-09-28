c14cfe3fbf76ac2d2bd626e80d98228d03e5dbe0
minh084 <minh084@stanford.edu>
2021-09-28 19:06:45 +0000

    update transfers

--- SQL/currentdx.sql
+++ SQL/currentdx.sql
@@ -1,14 +1,11 @@
 select distinct c.anon_id, c.pat_enc_csn_id_coded, 
-                dx.line, dx.dx_name, dx.primary, dx.chronic, dx.principal, dx.hospital_pl, dx.ed, dx.present_on_adm
+                dx.line, count(dx.dx_name), dx.primary, dx.chronic, dx.principal, dx.hospital_pl, dx.ed, dx.present_on_adm
 from
   `som-nero-phi-jonc101.triageTD.1_4_cohort_24hrpreadmit_diff_full_features` as c
 join
   `som-nero-phi-jonc101.shc_core.diagnosis_code` as dx
--- where
---   c.anon_id = dx.anon_id
---   and c.pat_enc_csn_id_coded = dx.pat_enc_csn_id_jittered
-
 ON 
     (c.anon_id = dx.anon_id and c.pat_enc_csn_id_coded = dx.pat_enc_csn_id_jittered)
-
+    
+GROUP by dx.dx_name
 -- WHERE (x.primary = 'Y')
