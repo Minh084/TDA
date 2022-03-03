6888bb6bbeafc7fc4bd105fb01cc1327947b0657
minh084 <minh084@stanford.edu>
2022-03-03 07:53:32 +0000

    check val data

--- shc2021/18_model_lgbm.sh
+++ shc2021/18_model_lgbm.sh
@@ -11,10 +11,10 @@ model=lightgbm
 
 cohort=14_cohort_final
 
-# label=first_label
+label=first_label
 # label=death_3hr_recent_label
 # label=death_6hr_recent_label
-label=death_9hr_recent_label
+# label=death_9hr_recent_label
 # label=death_12hr_recent_label
 # label=death_24hr_recent_label
 
